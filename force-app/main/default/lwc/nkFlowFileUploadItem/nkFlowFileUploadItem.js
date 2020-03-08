2fdeb59e1fc4285f77f35b68f59d35d171453167
Scott McClung <scott@nerdmagik.com>
2020-03-08 08:44:00 -0700

    Compensated for ContentDocument record being returned early with some unexpected null values.

--- force-app/main/default/lwc/nkFlowFileUploadItem/nkFlowFileUploadItem.js
+++ force-app/main/default/lwc/nkFlowFileUploadItem/nkFlowFileUploadItem.js
@@ -1,33 +1,37 @@
-import {LightningElement, api, track} from 'lwc'
-import {deleteRecord} from 'lightning/uiRecordApi'
-import TITLE from '@salesforce/schema/ContentDocument.Title'
-import FILEEXTENSION from '@salesforce/schema/ContentDocument.FileExtension'
-import FILETYPE from '@salesforce/schema/ContentDocument.FileType'
-import CONTENTSIZE from '@salesforce/schema/ContentDocument.ContentSize'
+import { LightningElement, api, track } from "lwc"
+import { deleteRecord } from "lightning/uiRecordApi"
+import TITLE from "@salesforce/schema/ContentDocument.Title"
+import FILEEXTENSION from "@salesforce/schema/ContentDocument.FileExtension"
+import FILETYPE from "@salesforce/schema/ContentDocument.FileType"
+import CONTENTSIZE from "@salesforce/schema/ContentDocument.ContentSize"
 
 export default class FlowFileUploadItem extends LightningElement {
     @api recordId
     @track record
     @track showSpinner
-    @track error;
+    @track error
 
     fields = [TITLE, FILEEXTENSION, FILETYPE, CONTENTSIZE]
 
     get filename() {
-        if (!this.record) return ''
-        return this.record.Title.value + '.' + this.record.FileExtension.value
+        if (this.record && this.record.hasOwnProperty('Title') && this.record.hasOwnProperty('FileExtension')) {
+            return `${this.record.Title.value}.${this.record.FileExtension.value}`
+        }
+        return ""
     }
 
     get filesize() {
-        if (!this.record) return 0
-        return this.record.ContentSize.value
+        if (this.record && this.record.hasOwnProperty('ContentSize')) {
+            return this.record.ContentSize.value
+        }
+        return 0
     }
 
     get formattedFilesize() {
         if (this.filesize < 1024) {
-            return this.filesize + ' Bytes'
+            return this.filesize + " Bytes"
         }
-        return Math.floor(this.filesize / 1024) + ' KB'
+        return Math.floor(this.filesize / 1024) + " KB"
     }
 
     connectedCallback() {
@@ -35,24 +39,29 @@ export default class FlowFileUploadItem extends LightningElement {
     }
 
     onRecordLoaded(e) {
-        const record = (e.detail && e.detail.records && e.detail.records[this.recordId])
+        const record =
+            e.detail && e.detail.records && e.detail.records[this.recordId]
         this.record = record.fields
         this.disableSpinner()
     }
 
     deleteFile(e) {
         this.enableSpinner()
-        deleteRecord(this.recordId).then(record => {
-            this.dispatchEvent(new CustomEvent('delete', {
-                detail: this.recordId
-            }))
-        }).catch(error => {
-            console.log('File error: ', JSON.stringify(error))
-            if(error.body && error.body.message) {
-                this.error = error.body.message
-            }
-            this.disableSpinner()
-        })
+        deleteRecord(this.recordId)
+            .then(record => {
+                this.dispatchEvent(
+                    new CustomEvent("delete", {
+                        detail: this.recordId
+                    })
+                )
+            })
+            .catch(error => {
+                console.log("File error: ", JSON.stringify(error))
+                if (error.body && error.body.message) {
+                    this.error = error.body.message
+                }
+                this.disableSpinner()
+            })
     }
 
     enableSpinner() {
